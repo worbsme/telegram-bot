edfc62e07429083688816ce9c796c84ee2cc33a6
worbsme <worbsme@gmail.com>
2025-06-29 11:24:10 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import telebot
+import os
+
+TOKEN = os.environ.get("BOT_TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(func=lambda message: message.new_chat_members or message.left_chat_member)
+def handle_join_leave(message):
+    bot.delete_message(message.chat.id, message.message_id)
+
+print("🤖 Bot is running...")
+bot.polling()
